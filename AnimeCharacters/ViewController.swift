b2bee085c696c8bcbfe297c47458a8043ba6afc6
Miguel Nery <nery.mik@gmail.com>
2020-06-27 14:56:17 -0300

    basic display character on screen

--- AnimeCharacters/ViewController.swift
+++ AnimeCharacters/ViewController.swift
@@ -1,9 +1,47 @@
 import UIKit
+import SnapKit
+import Combine
 
 class ViewController: UIViewController {
-
+    private let label = UILabel(frame: .zero)
+    private let decodableFetcher: DecodableFetcher
+    private var sub: AnyCancellable?
+    
+    init(decodableFetcher: DecodableFetcher = DefaultDecodableFetcher()) {
+        self.decodableFetcher = decodableFetcher
+        
+        super.init(nibName: nil, bundle: nil)
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        sub = decodableFetcher.fetchDecodable(from: .getCharacter(traitCount: 3))
+            .sink(receiveCompletion: {_ in}, receiveValue: { (value: Character) in
+                DispatchQueue.main.async {
+                    self.label.text = value.description
+                }
+        })
+    }
+}
+
+extension ViewController: ViewCode {
+    func addViews() {
+        view.addSubview(label)
+    }
+    
+    func addConstraints() {
+        label.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+    
+    func additionalSetup() {
+        view.backgroundColor = .white
     }
 }
 
