cf8cad279e269478aea16c98c36c1dfec1b7196f
Miguel Nery <nery.mik@gmail.com>
2020-06-27 01:29:46 -0300

    setup for viewcode + update shitty gitignore

--- AnimeCharacters/ViewController.swift
+++ AnimeCharacters/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  AnimeCharacters
-//
-//  Created by miguel.horta.nery on 26/06/20.
-//  Copyright © 2020 miguel.horta.nery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -13,7 +5,5 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
 }
 
