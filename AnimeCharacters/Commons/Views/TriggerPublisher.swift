1d3a39d8f915c62e71b4dd00e9374d4ceef67b69
Miguel Nery <nery.mik@gmail.com>
2020-06-30 21:42:43 -0300

    refactor description flux with DescriptionButton screen

---
+++ AnimeCharacters/Commons/Views/TriggerPublisher.swift
@@ -0,0 +1,9 @@
+//
+//  TriggerPublisher..swift
+//  AnimeCharacters
+//
+//  Created by Miguel Nery on 30/06/20.
+//  Copyright © 2020 miguel.horta.nery. All rights reserved.
+//
+
+import Foundation
---
+++ AnimeCharacters/Screens/DescriptionButton/DescriptionButtonViewController.swift
@@ -0,0 +1,9 @@
+//
+//  DescriptionButtonViewController.swift
+//  AnimeCharacters
+//
+//  Created by Miguel Nery on 30/06/20.
+//  Copyright © 2020 miguel.horta.nery. All rights reserved.
+//
+
+import Foundation
